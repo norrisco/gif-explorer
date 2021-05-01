f0f59b8004214f6e866706199c667e5164ffe554
Norris Co <53540356+norrisco@users.noreply.github.com>
2021-04-30 23:44:50 -0400

    Updated No Gifs Found

--- src/Components/NoGifsFound.js
+++ src/Components/NoGifsFound.js
@@ -1,8 +1,9 @@
 const NoGifsFound = () => {
     return (
-        <>
+        <div style={{width: '200px', color: '#EAEAEA'}}>
             <h1>No Gifs Found</h1>
-        </>
+            <h4>Please try another search.</h4>
+        </div>
     )
 }
 

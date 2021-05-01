bd37162c004c65e52013d849ebe3b759476eee87
Norris Co <53540356+norrisco@users.noreply.github.com>
2021-04-30 23:08:15 -0400

    Updated Header syle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,9 @@ import { FileGifOutlined } from '@ant-design/icons';
 const Header = () => {
     return (
         <div className='headerContainer'>
-            <FileGifOutlined style={{fontSize: '64px'}}/>
-            <h1>
-                Explorer
+            {/* <FileGifOutlined style={{fontSize: '64px'}}/> */}
+            <h1 className='mainTitle'>
+                GIF Explorer
             </h1>
         </div>
     )

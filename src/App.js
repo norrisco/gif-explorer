eccec0b4f718f501d2047bf2c07eb11600f69a99
Norris Co <53540356+norrisco@users.noreply.github.com>
2021-04-30 23:57:45 -0400

    Changed api key

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
   }, [])
 
   const searchGiphy = (query = 'dog') => {
-    axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=${query_limit}&api_key=${api_key2}/`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=${query_limit}&api_key=${api_key}/`)
       .then(response => {
         setGifData(response.data.data);
         setLoading(false);
